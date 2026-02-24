19e0acd28f35cb18920e3678755b549d0b2b64c7
junying2025 <junying.wang@gigadevice.com>
2026-02-24 15:08:33 +0800

    gd32f30x: add GD32F307 HAL support

--- common_include/gd32_crc.h
+++ common_include/gd32_crc.h
@@ -14,6 +14,8 @@
 #include <gd32e50x_crc.h>
 #elif defined(CONFIG_SOC_SERIES_GD32E51X)
 #include <gd32e51x_crc.h>
+#elif defined(CONFIG_SOC_SERIES_GD32F30X)
+#include <gd32f30x_crc.h>
 #elif defined(CONFIG_SOC_SERIES_GD32F3X0)
 #include <gd32f3x0_crc.h>
 #elif defined(CONFIG_SOC_SERIES_GD32F403)
