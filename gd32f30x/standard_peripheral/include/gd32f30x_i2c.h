0ef8f99fdeade931218bd578f552b492da43ab43
junying2025 <junying.wang@gigadevice.com>
2026-02-28 09:42:36 +0800

    i2c: add I2CCLK_FM_MIN and I2CCLK_FM_PLUS_MIN macros to gd32f30x_i2c.h

--- gd32f30x/standard_peripheral/include/gd32f30x_i2c.h
+++ gd32f30x/standard_peripheral/include/gd32f30x_i2c.h
@@ -284,6 +284,8 @@ typedef enum
 /* I2C clock frequency, MHz */
 #define I2CCLK_MAX                    ((uint32_t)0x0000003CU)                  /*!< i2cclk maximum value */
 #define I2CCLK_MIN                    ((uint32_t)0x00000002U)                  /*!< i2cclk minimum value */
+#define I2CCLK_FM_MIN                 ((uint32_t)0x00000008U)                  /*!< i2cclk minimum value for fast mode */
+#define I2CCLK_FM_PLUS_MIN            ((uint32_t)0x00000018U)                  /*!< i2cclk minimum value for fast mode plus */
 
 /* function declarations */
 /* initialization functions */
